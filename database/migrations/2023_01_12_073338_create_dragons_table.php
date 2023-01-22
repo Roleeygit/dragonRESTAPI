edc7bd482b87aa4535057f8aab2c2fae4d45512c
Roleeygit <juhaszroli2001@gmail.com>
2023-01-22 20:57:53 +0100

    migration fix

--- database/migrations/2023_01_12_073338_create_dragons_table.php
+++ database/migrations/2023_01_12_073338_create_dragons_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("age");
-            // $table->foreignId("color_id");
+            $table->foreignId("color_id");
             $table->foreign('color_id')->references('id')->on('colors');
         });
     }
